42b3ebda137b95fb09390dc4970845ad561fa3d4
Prejin Senan <93463606+prejin2310@users.noreply.github.com>
2022-02-17 08:12:50 +0530

    Pgm 11

---
+++ CO2/lambda-fn.py
@@ -0,0 +1,6 @@
+square = lambda s : s*s
+rectangle = lambda l,b : l*b
+triangle = lambda b,h : (b*h)/2
+print('Area of square',square(10))
+print('Area of triangle',triangle(7,10))
+print('Area of rectangle',rectangle(9,5))
